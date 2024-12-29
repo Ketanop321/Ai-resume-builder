89312db9352f4a7231047474f5a014fa15248253
Gamerking177 <ketanop473@gmail.com>
2024-12-29 21:33:11 +0530

    hehe

--- src/dashboard/resume/components/RichTextEditor.jsx
+++ src/dashboard/resume/components/RichTextEditor.jsx
@@ -3,7 +3,7 @@ import { ResumeInfoContext } from '@/context/ResumeInfoContext';
 import { Brain, LoaderCircle } from 'lucide-react';
 import React, { useContext, useState } from 'react'
 import { BtnBold, BtnBulletList, BtnClearFormatting, BtnItalic, BtnLink, BtnNumberedList, BtnStrikeThrough, BtnStyles, BtnUnderline, Editor, EditorProvider, HtmlButton, Separator, Toolbar } from 'react-simple-wysiwyg'
-import { AIChatSession } from './../../../../service/AIModal';
+import { AIChatSession } from '../../../../service/AIModel';
 import { toast } from 'sonner';
 const PROMPT='position titile: {positionTitle} , Depends on position title give me 5-7 bullet points for my experience in resume (Please do not add experince level and No JSON array) , give me result in HTML tags'
 function RichTextEditor({onRichTextEditorChange,index,defaultValue}) {
